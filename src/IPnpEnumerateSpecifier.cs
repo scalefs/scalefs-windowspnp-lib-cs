84cef70727776fff6f2f1f34cdd873e6e2ad453a
kit-scalefs <154656609+kit-scalefs@users.noreply.github.com>
2023-12-30 16:43:38 -0400

    Add enums, errors and PnpDeviceNodeInfo

---
+++ src/IPnpEnumerateSpecifier.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) ScaleFS LLC; used with permission
+// Licensed under the MIT License
+
+using System;
+
+namespace ScaleFS.WindowsPnp;
+
+public interface IPnpEnumerateSpecifier
+{
+     public record AllDevices() : IPnpEnumerateSpecifier;
+     public record DeviceInterfaceClassGuid(Guid InterfaceClassGuid) : IPnpEnumerateSpecifier;
+     public record DeviceSetupClassGuid(Guid SetupClassGuid) : IPnpEnumerateSpecifier;
+     public record PnpDeviceInstanceId(String InstanceId, Guid? InterfaceClassGuid) : IPnpEnumerateSpecifier;
+     public record PnpEnumeratorId(string EnumeratorId) : IPnpEnumerateSpecifier;
+}
---
+++ src/PnpDeviceNodeInfo.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) ScaleFS LLC; used with permission
+// Licensed under the MIT License
+
+using System.Collections.Generic;
+
+namespace ScaleFS.WindowsPnp;
+
+public struct PnpDeviceNodeInfo
+{
+     // device instance id (applies to all devices)
+     public string DeviceInstanceId { get; init; }
+     // NOTE: the container_id is probably unique, but is _not_ guaranteed to be unique; see: https://techcommunity.microsoft.com/t5/microsoft-usb-blog/how-to-generate-a-container-id-for-a-usb-device-part-2/ba-p/270726
+     // NOTE: the container_id should be available for virutally all devices, but perhaps _not_ for root hubs (and maybe not for hubs at all...TBD)
+     public string? ContainerId { get; init; }
+     //
+     // device instance properties (optional; these should be available for all devices)
+     public Dictionary<PnpDevicePropertyKey, IPnpDevicePropertyValue>? DeviceInstanceProperties { get; init; }
+     //
+     // device setup class properties (optional) (also, they are available for most devices but not ALL devices)
+     public Dictionary<PnpDevicePropertyKey, IPnpDevicePropertyValue>? DeviceSetupClassProperties { get; init; }
+     //
+     // device path (only applies to device interfaces; will be None otherwise)
+     public string? DevicePath { get; init; }
+     // interface properties (optional) (also, they only apply to device interfaces; will be None otherwise)
+     public Dictionary<PnpDevicePropertyKey, IPnpDevicePropertyValue>? DeviceInterfaceProperties { get; init; }
+     // interface class properties (optional) (also, they only apply to device interfaces; will be None otherwise)
+     public Dictionary<PnpDevicePropertyKey, IPnpDevicePropertyValue>? DeviceInterfaceClassProperties { get; init; }
+}
